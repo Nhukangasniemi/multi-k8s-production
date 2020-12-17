44b2b2e10da8958e4975fd761f50761c8ee9f92b
Nhu Kangasniemi <nhu.kangasniemi@symbio.com>
2020-12-17 12:11:40 +0200

    Install and configure skaffold for project

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Version Kubernetes Production
+            Version Kubernetes Production - Skaffold Modified
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
